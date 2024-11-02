0dff629c14c8d2789e3062c37af240922f9a3e37
ghalass <msghalas@gmail.com>
2024-11-02 12:29:39 +0000

    Home Page Design

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
 import React from "react";
-import CloudImage from "../../../public/cloud-hosting.png";
 
 const AboutPage = () => {
   return (
     <section className="fix-height container m-auto">
-      <h1 className="text-3xl font-bold text-gray-800 p-5">About Page</h1>
-      <div>
-        <Image src={CloudImage} alt="cloudimage" height={500} width={500} />
-      </div>
+      <h1 className="text-3xl font-bold text-gray-800 p-5">About This App</h1>
+      <p className="px-5 text-gray-600 text-xl">
+        The best web hosting solution for your online success
+      </p>
     </section>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import Hero from "@/components/home/Hero";
+import WebHostingPlan from "@/components/home/WebHostingPlan";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to NextJs</p>
-    </div>
+    <section className="">
+      <Hero />
+      <h2 className="text-center mt-10 text-3xl font-bold">
+        Choose Your Web Hosting Plan
+      </h2>
+      <div className="container m-auto flex justify-center items-center my-7 flex-wrap md:gap-7">
+        <WebHostingPlan />
+        <WebHostingPlan />
+        <WebHostingPlan />
+      </div>
+    </section>
   );
 };
 
