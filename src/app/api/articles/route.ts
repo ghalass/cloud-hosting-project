81f2cfb245a3708d8dc88811a93000a59f039d04
ghalass <msghalas@gmail.com>
2024-11-02 17:50:07 +0000

    POST Route Handler

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -1,4 +1,7 @@
 import { articles } from "@/utils/data";
+import { CreateArticleDto } from "@/utils/dtos";
+import { Article } from "@/utils/types";
+import { createArticleSchema } from "@/utils/validationSchema";
 import { NextRequest, NextResponse } from "next/server";
 
 /**
@@ -7,6 +10,32 @@ import { NextRequest, NextResponse } from "next/server";
  * @description Get All Articles
  * @access public
  */
-export function GET(request: NextRequest) {
+export function GET() {
   return NextResponse.json(articles, { status: 200 });
+}
+
+/**
+ * @method POST
+ * @route ~/api/articles
+ * @description Create New Article
+ * @access public
+ */
+export async function POST(request: NextRequest) {
+  const body = (await request.json()) as CreateArticleDto;
+
+  const validation = createArticleSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(
+      { message: validation.error.errors[0].message },
+      { status: 400 }
+    );
+
+  const newArticle: Article = {
+    title: body.body,
+    body: body.body,
+    id: articles.length + 1,
+    userId: 200,
+  };
+  articles.push(newArticle);
+  return NextResponse.json(newArticle, { status: 201 });
 }
