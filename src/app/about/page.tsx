834b6be0fefda8ec4eef237c2d654c7339950d7a
ghalass <msghalas@gmail.com>
2024-11-02 11:46:51 +0000

    Image Component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
 import React from "react";
+import CloudImage from "../../../public/cloud-hosting.png";
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <section className="fix-height container m-auto">
+      <h1 className="text-3xl font-bold text-gray-800 p-5">About Page</h1>
+      <div>
+        <Image src={CloudImage} alt="cloudimage" height={500} width={500} />
+      </div>
+    </section>
   );
 };
 
