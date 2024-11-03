04727ca68d238ee2b3a759a9b2b16e479689afbb
ghalass <msghalas@gmail.com>
2024-11-03 21:00:26 +0000

    Admin Comments Table

---
+++ src/apiCalls/adminApiCall.ts
@@ -0,0 +1,11 @@
+import { DOMAIN } from "@/utils/constants";
+import { Comment } from "@prisma/client";
+
+// get all comments
+export async function getAllComments(token: string): Promise<Comment> {
+  const response = await fetch(`${DOMAIN}/api/comments`, {
+    headers: { Cookie: `jwtToken=${token}` },
+  });
+  if (!response.ok) throw new Error("Failed to fetch comments");
+  return await response.json();
+}
--- src/app/admin/comments-table/page.tsx
+++ src/app/admin/comments-table/page.tsx
@@ -1,15 +1,46 @@
+import { getAllComments } from "@/apiCalls/adminApiCall";
 import { verifyTokenForPage } from "@/utils/verifyToken";
+import { Comment } from "@prisma/client";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import DeleteCommentButton from "./DeleteCommentButton";
 
-const AdminCommentsTable = () => {
+const AdminCommentsTable = async () => {
   const token = cookies().get("jwtToken")?.value;
   if (!token) redirect("/");
 
   const payload = verifyTokenForPage(token);
   if (payload?.isAdmin === false) redirect("/");
 
-  return <div>AdminCommentsTable</div>;
+  const comments: Comment[] = await getAllComments(token);
+
+  return (
+    <section className="p-5">
+      <h1 className="mb-7 text-2xl font-semibold text-gray-700 ">Comments</h1>
+      <table className="table w-full text-left">
+        <thead className="border-t-2 border-b-2 border-gray-500 text-xl">
+          <tr>
+            <th className="p-2">Comment</th>
+            <th className="hidden lg:inline-block p-3">Created At</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {comments.map((comment) => (
+            <tr key={comment.id} className="border-b border-t border-gray-300">
+              <td className="p-3 text-gray-700">{comment.text}</td>
+              <td className="p-3 text-gray-700 font-normal hidden lg:inline-block">
+                {new Date(comment.createdAt).toDateString()}
+              </td>
+              <td className="p-3 text-gray-700">
+                <DeleteCommentButton commentId={comment.id} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
 };
 
 export default AdminCommentsTable;
